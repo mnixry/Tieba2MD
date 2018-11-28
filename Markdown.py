1395e6eafb2c349697104c0123ec20beb687c89e
mnixry <admin@mnixry.cn>
2018-11-28 15:21:43 +0800

    changed the Markdown.py

--- Markdown.py
+++ Markdown.py
@@ -1,3 +1,9 @@
 #/usr/local/env python3
-#__coding:utf-8__
+#__coding:utf-8 __
+'''
+There need something to introduction it.
+Project need to new.
+
+HTML to Markdown module.
+followed GPL license
 '''
