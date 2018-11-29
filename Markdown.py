8fecc37b49a2a04f81146a54dd380883e7704088
mnixry <admin@mnixry.cn>
2018-11-29 20:49:45 +0800

    changed Markdown.py

--- Markdown.py
+++ Markdown.py
@@ -1,9 +1,19 @@
 #/usr/local/env python3
 #__coding:utf-8 __
 '''
-There need something to introduction it.
-Project need to new.
-
 HTML to Markdown module.
 followed GPL license
 '''
+
+import html,json,re,time,os
+
+class Markdown():
+
+    def convert(html_res, compatible=True):
+        if compatible:
+            pass
+        else:
+            pass
+        
+        #<div style="text-align:right;font-size:12px;filter:alpha(Opacity=30);-moz-opacity:0.3;opacity: 0.3;">texthere</div>
+        
