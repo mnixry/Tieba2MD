824c5d2c03f6ef12da141c7ed8cf0fa0aaa69d07
mnixry <admin@mnixry.cn>
2018-11-30 15:21:53 +0800

    changed Markdown.py(add some regex)

--- Markdown.py
+++ Markdown.py
@@ -6,6 +6,12 @@ followed GPL license
 '''
 
 import html,json,re,time,os
+from lxml import etree
+
+div_tag = re.compile('<\s*div[^>]*>[^<]*')
+a_tag = re.compile('<\s*a[^>]*>[^<]*')
+br_tag = re.compile('<br\s*?/?>')
+img_tag = re.compile('')
 
 class Markdown():
 
@@ -14,6 +20,12 @@ class Markdown():
             pass
         else:
             pass
+
+        html_res = div_tag.sub('',html_res)
+        html_res = html_res.replace('</div>','',1)
+        html_res = br_tag.sub('\n',html_res)
+        html_res = a_tag.sub('',html_res)
+        html_res = html_res.replace('</a>','')
         
         #<div style="text-align:right;font-size:12px;filter:alpha(Opacity=30);-moz-opacity:0.3;opacity: 0.3;">texthere</div>
         
